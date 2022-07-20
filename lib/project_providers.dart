347182844f1c0ed7cff2722fe916cf905bdf232a
Yaser <yaser@MacBook-Air-de-Yaser.local>
2022-07-20 12:05:11 -0600

    changes on the providers file

--- lib/project_providers.dart
+++ lib/project_providers.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final AWClientProvider = Provider<Client>((ref) {
   Client client = Client();
-  client.setEndpoint('https://localhost/v1').setProject('62d07cbf622684975b28');
+  client
+      .setEndpoint('https://192.168.100.33:8081/v1')
+      .setProject('62d07cbf622684975b28');
   return client;
 });
 
